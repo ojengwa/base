ccd59d7ab2d45f0d6fe4e181bb06311ca4f1209a
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:26 +0100

    commit changes to server/core/settings/dev.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/core/settings/dev.py
@@ -0,0 +1,51 @@
+from .base import *  # NOQA (ignore all errors on this line)
+
+
+DEBUG = True
+
+PAGE_CACHE_SECONDS = 1
+
+DATABASES['extra'] = env.db('SQLITE_URL', default='sqlite:////tmp/db.sqlite3')
+
+
+REST_FRAMEWORK['EXCEPTION_HANDLER'] = 'django_rest_logger.handlers.rest_exception_handler'  # NOQA (ignore all errors on this line)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['django_rest_logger_handler'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'django_rest_logger_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['django_rest_logger_handler'],
+            'propagate': False,
+        },
+        'django_rest_logger': {
+            'level': 'DEBUG',
+            'handlers': ['django_rest_logger_handler'],
+            'propagate': False,
+        },
+    },
+}
+
+DEFAULT_LOGGER = 'django_rest_logger'
+
+LOGGER_EXCEPTION = DEFAULT_LOGGER
+LOGGER_ERROR = DEFAULT_LOGGER
+LOGGER_WARNING = DEFAULT_LOGGER
