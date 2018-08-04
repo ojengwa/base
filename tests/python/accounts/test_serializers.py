238a07c18de0953aafb4a5e01ab4fe33f89860e3
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:27 +0100

    commit changes to tests/python/accounts/test_serializers.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ tests/python/accounts/test_serializers.py
@@ -0,0 +1,96 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from server.apps.accounts.serializers import (
+    UserRegistrationSerializer, UserSerializer)
+from server.helpers.tests.utils import CustomTestCase
+from .test_models import UserFactory
+
+
+class UserRegistrationSerializerTest(CustomTestCase, APITestCase):
+    INVALID_DATA_DICT = [
+        {'data': {'email': 'test1@mailinator.com',
+                  'first_name': 'test',
+                  'last_name': 'user',
+                  'password': 'test'},
+         'error': (
+            'email', ['Please use a different email address provider.']),
+         'label': 'Invalid email.',
+         'method': 'POST',
+         'status': status.HTTP_400_BAD_REQUEST
+         },
+
+        {'data': {'email': 'test1@gmail',
+                  'first_name': 'test',
+                  'last_name': 'user',
+                  'password': 'test'},
+         'error': ('email', ['Enter a valid email address.']),
+         'label': 'Bad email format.',
+         'method': 'POST',
+         'status': status.HTTP_400_BAD_REQUEST
+         },
+    ]
+    VALID_DATA_DICT = [
+        {'email': 'emailsuccess@gmail.com',
+         'first_name': 'test',
+         'last_name': 'user',
+         'password': 'test'},
+    ]
+
+    def setUp(self):
+        self.required_fields = ['email', 'first_name', 'last_name', 'password']
+        self.not_required_fields = ['id']
+        self.user = UserFactory.create(
+            email='emailwilllogininserializer@mydomain.com')
+
+    def test_fields(self):
+        serializer = UserRegistrationSerializer()
+        self.assert_fields_required(True, serializer, self.required_fields)
+        self.assert_fields_required(
+            False, serializer, self.not_required_fields)
+        self.assertEqual(len(serializer.fields), len(
+            self.required_fields) + len(self.not_required_fields))
+
+    def test_invalid_data(self):
+        serializer = UserRegistrationSerializer
+        self.assert_invalid_data(serializer, self.INVALID_DATA_DICT)
+
+    def test_validate_success(self):
+        serializer = UserRegistrationSerializer
+        self.assert_valid_data(serializer, self.VALID_DATA_DICT)
+
+
+class UserSerializerTest(CustomTestCase, APITestCase):
+    INVALID_DATA_DICT = [
+        {'data': {'email': 'emailwilllogin@mydomain.com',
+                  'first_name': 'test', 'last_name': ''},
+         'error': ('last_name', ['This field may not be blank.']),
+         'label': 'Last name is required',
+         'method': 'POST',
+         'status': status.HTTP_400_BAD_REQUEST
+         },
+    ]
+    VALID_DATA_DICT = [
+        {'email': 'emailwilllogin@mydomain.com',
+            'first_name': 'test', 'last_name': 'test'},
+    ]
+
+    def setUp(self):
+        self.required_fields = ['email', 'first_name', 'last_name']
+        self.not_required_fields = []
+
+    def test_fields(self):
+        serializer = UserSerializer()
+        self.assert_fields_required(True, serializer, self.required_fields)
+        self.assert_fields_required(
+            False, serializer, self.not_required_fields)
+        self.assertEqual(len(serializer.fields), len(
+            self.required_fields) + len(self.not_required_fields))
+
+    def test_invalid_data(self):
+        serializer = UserSerializer
+        self.assert_invalid_data(serializer, self.INVALID_DATA_DICT)
+
+    def test_validate_success(self):
+        serializer = UserSerializer
+        self.assert_valid_data(serializer, self.VALID_DATA_DICT)
