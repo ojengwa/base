58d03753bae56965eb4e3c1170e6f59586e1568b
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:22 +0100

    commit changes to server/apps/activities/views.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/apps/activities/views.py
@@ -0,0 +1,26 @@
+from actstream.models import Action
+from rest_framework.response import Response
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from .serializers import ActionSerializer
+
+
+class ActivitiesViewSet(ReadOnlyModelViewSet):
+    serializer_class = ActionSerializer
+    queryset = Action.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        """list as in DRF but filter the queryset with kwargs.
+
+        """
+        queryset = self.queryset.filter(
+            target_content_type=kwargs['content_type']).filter(
+            target_object_id=kwargs['pk'])
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
