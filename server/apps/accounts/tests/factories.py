713ed4d036a34c257bfe19e8426330090bb87921
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:21 +0100

    commit changes to server/apps/accounts/tests/factories.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/apps/accounts/tests/factories.py
@@ -0,0 +1,31 @@
+import factory
+from allauth.account.models import EmailAddress
+
+from server.apps.tenants.tests.factories import TenantFactory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Sequence(lambda n: 'user-{0}'.format(n))
+    email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n))
+    password = factory.PostGenerationMethodCall('set_password', 'password')
+
+    class Meta:
+        model = 'users.User'
+        django_get_or_create = ('username', )
+
+
+class VerifiedUserFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    email = factory.LazyAttribute(lambda a: a.user.email)
+    verified = True
+
+    class Meta:
+        model = EmailAddress
+
+
+class UserTenantRelationshipFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    tenant = factory.SubFactory(TenantFactory)
+
+    class Meta:
+        model = 'users.UserTenantRelationship'
