5ca382ab4eddca2489e05b65e6d866d57e043955
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:23 +0100

    commit changes to server/apps/comments/tests/factories.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/apps/comments/tests/factories.py
@@ -0,0 +1,26 @@
+import factory
+
+from django.contrib.contenttypes.models import ContentType
+
+from server.apps.users.tests.factories import UserFactory
+
+
+class CommentFactory(factory.django.DjangoModelFactory):
+
+    author = factory.SubFactory(UserFactory)
+    object_id = factory.SelfAttribute('content_object.id')
+    content_type = factory.LazyAttribute(
+        lambda o: ContentType.objects.get_for_model(o.content_object))
+    content = factory.Sequence(lambda n: f"Comment #{n + 1}")
+
+    class Meta:
+        exclude = ['content_object']
+        abstract = True
+
+
+class CommentUserFactory(CommentFactory):
+    """A Factory for comments on users."""
+    content_object = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = 'comments.Comment'
