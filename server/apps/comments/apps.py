6e18cbf7d98175f383a9499968e0b1610e418c98
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:23 +0100

    commit changes to server/apps/comments/apps.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/apps/comments/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+
+
+class CommentsConfig(AppConfig):
+    name = 'comments'
+    verbose_name = "Comments"
+
+    def ready(self):
+        """If django-activity-stream
+         is installed, register the Comment model for usage."""
+        try:
+            from actstream import registry
+            registry.register(self.get_model('Comment'))
+        except ImportError:
+            pass
