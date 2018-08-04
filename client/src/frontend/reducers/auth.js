3637e167d17678f5465127cd4f1db8a3eb8dc1d0
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:17 +0100

    commit changes to client/src/frontend/reducers/auth.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ client/src/frontend/reducers/auth.js
@@ -0,0 +1,55 @@
+import {
+    AUTH_LOGIN_USER_REQUEST,
+    AUTH_LOGIN_USER_SUCCESS,
+    AUTH_LOGIN_USER_FAILURE,
+    AUTH_LOGOUT_USER
+} from '../constants';
+
+
+const initialState = {
+    token: null,
+    userName: null,
+    isAuthenticated: false,
+    isAuthenticating: false,
+    statusText: null
+};
+
+export default function authReducer(state = initialState, action) {
+    switch (action.type) {
+        case AUTH_LOGIN_USER_REQUEST:
+            return Object.assign({}, state, {
+                isAuthenticating: true,
+                statusText: null
+            });
+
+        case AUTH_LOGIN_USER_SUCCESS:
+            return Object.assign({}, state, {
+                isAuthenticating: false,
+                isAuthenticated: true,
+                token: action.payload.token,
+                userName: action.payload.user.email,
+                statusText: 'You have been successfully logged in.'
+            });
+
+        case AUTH_LOGIN_USER_FAILURE:
+            return Object.assign({}, state, {
+                isAuthenticating: false,
+                isAuthenticated: false,
+                token: null,
+                userName: null,
+                statusText: `Authentication Error: ${action.payload.status} - ${action.payload.statusText}`
+            });
+
+        case AUTH_LOGOUT_USER:
+            return Object.assign({}, state, {
+                isAuthenticated: false,
+                token: null,
+                userName: null,
+                statusText: 'You have been successfully logged out.'
+            });
+
+        default:
+            return state;
+    }
+}
+
