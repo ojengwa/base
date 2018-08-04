0a2043c5c43df60c17f4726e0d35b1d097e01cb9
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:24 +0100

    commit changes to server/apps/tenants/registration/urls.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/apps/tenants/registration/urls.py
@@ -0,0 +1,45 @@
+from allauth.account.views import email_verification_sent
+
+from django.conf.urls import url
+
+from .views import (
+    GetAllowedVerifyEmailView, InviteActivationView, InviteCreateView,
+    InviteRetrieveView, TenantRegisterView, TenantUserRegisterView)
+
+urlpatterns = [
+    url(r'^tenant/$', TenantRegisterView.as_view(),
+        name='tenant_rest_register'),
+    url(r'^tenant/(?P<tenant_name>[-:\w]+)/user/$',
+        TenantUserRegisterView.as_view(),
+        name='user_rest_register'),
+
+    url(r'^tenant/(?P<tenant_name>[-:\w]+)/invite/$',
+        InviteCreateView.as_view(),
+        name='rest_invite'),
+    url(r'^tenant/(?P<tenant_name>[-:\w]+)/invite/(?P<pk>[0-9a-f-]+)/$',
+        InviteRetrieveView.as_view(),
+        name='rest_invite_retrieve'),
+    url((r'^tenant/(?P<tenant_name>[-:\w]+)/'
+         r'invite/(?P<pk>[0-9a-f-]+)/activate/$'
+         ), InviteActivationView.as_view(),
+        name='rest_invite_activation'),
+
+    # FIXME Do we need this active?
+    # This url is used by django-allauth and empty TemplateView is
+    # defined just to allow reverse() call inside app, for example when email
+    # with verification link is being sent, then it's required to render email
+    # content.
+
+    # account_confirm_email - You should override this view to handle it in
+    # your API client somehow and then, send post to /verify-email/ endpoint
+    # with proper key.
+    # If you don't want to use API on that step, then just use ConfirmEmailView
+    # view from:
+    # url(r'^account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(),
+    #     name='account_confirm_email'),
+
+    url(r'^confirm-email/$', email_verification_sent,
+        name='account_email_verification_sent'),
+    url(r'^confirm-email/(?P<key>[-:\w]+)/$',
+        GetAllowedVerifyEmailView.as_view(), name='account_confirm_email'),
+]
