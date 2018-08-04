1b96e70044bb9ccf618d3daaf1286788e1816089
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:26 +0100

    commit changes to tests/js/actions/auth.specs.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ tests/js/actions/auth.specs.js
@@ -0,0 +1,202 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+
+import { expect } from 'chai';
+import nock from 'nock';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import * as TYPES from '../../../src/static/constants';
+import * as ACTIONS_AUTH from '../../../src/static/actions/auth';
+
+import { SERVER_URL } from '../../../src/static/utils/config';
+
+
+describe('Auth Actions:', () => {
+    afterEach(() => {
+        nock.cleanAll();
+    });
+
+    beforeEach(() => {
+        localStorage.removeItem('token');
+    });
+
+    it('authLoginUserSuccess should create LOGIN_USER_SUCCESS action', () => {
+        expect(ACTIONS_AUTH.authLoginUserSuccess('token', {})).to.eql({
+            type: TYPES.AUTH_LOGIN_USER_SUCCESS,
+            payload: {
+                token: 'token',
+                user: {}
+            }
+        });
+    });
+
+    it('authLoginUserFailure should create LOGIN_USER_FAILURE action', () => {
+        expect(ACTIONS_AUTH.authLoginUserFailure(404, 'Not found')).to.eql({
+            type: TYPES.AUTH_LOGIN_USER_FAILURE,
+            payload: {
+                status: 404,
+                statusText: 'Not found'
+            }
+        });
+    });
+
+    it('authLoginUserRequest should create LOGIN_USER_REQUEST action', () => {
+        expect(ACTIONS_AUTH.authLoginUserRequest()).to.eql({
+            type: TYPES.AUTH_LOGIN_USER_REQUEST
+        });
+    });
+
+    it('authLogout should create LOGOUT_USER action', () => {
+        expect(ACTIONS_AUTH.authLogout()).to.eql({
+            type: TYPES.AUTH_LOGOUT_USER
+        });
+    });
+
+    it('authLogoutAndRedirect should create authLogout and pushState actions', (done) => {
+        const expectedActions = [
+            {
+                type: TYPES.AUTH_LOGOUT_USER
+            }, {
+                type: '@@router/CALL_HISTORY_METHOD',
+                payload: {
+                    method: 'push',
+                    args: [
+                        '/login'
+                    ]
+                }
+            }
+        ];
+
+        const middlewares = [thunk];
+        const mockStore = configureStore(middlewares);
+        const store = mockStore({});
+
+        store.dispatch(ACTIONS_AUTH.authLogoutAndRedirect())
+            .then(() => {
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            }).then(done).catch(done);
+    });
+
+    it('authLoginUser should create LOGIN_USER_REQUEST, LOGIN_USER_SUCCESS, ' +
+        'and PUSH_STATE actions when API returns 200', (done) => {
+        const expectedActions = [
+            {
+                type: TYPES.AUTH_LOGIN_USER_REQUEST
+            }, {
+                type: TYPES.AUTH_LOGIN_USER_SUCCESS,
+                payload: {
+                    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IlRlc3QgVXNlciJ9.J6n4-v0I85' +
+                    'zk9MkxBHroZ9ZPZEES-IKeul9ozxYnoZ8',
+                    user: {}
+                }
+            }, {
+                type: '@@router/CALL_HISTORY_METHOD',
+                payload: {
+                    method: 'push',
+                    args: [
+                        '/'
+                    ]
+                }
+            }
+        ];
+
+        nock(SERVER_URL)
+            .post('/api/v1/accounts/login/')
+            .reply(200, {
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6IlRlc3QgVXNlciJ9.J6n4-v0I85zk9Mk' +
+                'xBHroZ9ZPZEES-IKeul9ozxYnoZ8',
+                user: {}
+            });
+
+        const middlewares = [thunk];
+        const mockStore = configureStore(middlewares);
+        const store = mockStore({});
+
+        store.dispatch(ACTIONS_AUTH.authLoginUser())
+            .then(() => {
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            }).then(done).catch(done);
+    });
+
+    it('authLoginUser should create LOGIN_USER_REQUEST and LOGIN_USER_FAILURE actions when API returns 401', (done) => {
+        const expectedActions = [
+            {
+                type: TYPES.AUTH_LOGIN_USER_REQUEST
+            }, {
+                type: TYPES.AUTH_LOGIN_USER_FAILURE,
+                payload: {
+                    status: 401,
+                    statusText: 'Unauthorized'
+                }
+            }
+        ];
+
+        nock(SERVER_URL)
+            .post('/api/v1/accounts/login/')
+            .reply(401, { non_field_errors: ['Unauthorized'] });
+
+        const middlewares = [thunk];
+        const mockStore = configureStore(middlewares);
+        const store = mockStore({});
+
+        store.dispatch(ACTIONS_AUTH.authLoginUser())
+            .then(() => {
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            }).then(done).catch(done);
+    });
+
+    it('authLoginUser should create LOGIN_USER_REQUEST and LOGIN_USER_FAILURE actions when API returns 500', (done) => {
+        const expectedActions = [
+            {
+                type: TYPES.AUTH_LOGIN_USER_REQUEST
+            }, {
+                type: TYPES.AUTH_LOGIN_USER_FAILURE,
+                payload: {
+                    status: 500,
+                    statusText: 'A server error occurred while sending your data!'
+                }
+            }
+        ];
+
+        nock(SERVER_URL)
+            .post('/api/v1/accounts/login/')
+            .reply(500);
+
+        const middlewares = [thunk];
+        const mockStore = configureStore(middlewares);
+        const store = mockStore({});
+
+        store.dispatch(ACTIONS_AUTH.authLoginUser())
+            .then(() => {
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            }).then(done).catch(done);
+    });
+
+    it('authLoginUser should create LOGIN_USER_REQUEST and LOGIN_USER_FAILURE actions when API ' +
+        'has no response', (done) => {
+        const expectedActions = [
+            {
+                type: TYPES.AUTH_LOGIN_USER_REQUEST
+            }, {
+                type: TYPES.AUTH_LOGIN_USER_FAILURE,
+                payload: {
+                    status: 'Connection Error',
+                    statusText: 'An error occurred while sending your data!'
+                }
+            }
+        ];
+
+        nock(SERVER_URL)
+            .post('/api/v1/accounts/login/')
+            .reply();
+
+        const middlewares = [thunk];
+        const mockStore = configureStore(middlewares);
+        const store = mockStore({});
+
+        store.dispatch(ACTIONS_AUTH.authLoginUser())
+            .then(() => {
+                expect(store.getActions()).to.deep.equal(expectedActions);
+            }).then(done).catch(done);
+    });
+});
