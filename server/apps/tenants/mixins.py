7c2b4e4eb65405bcf328942f6654dbe073cd730c
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:24 +0100

    commit changes to server/apps/tenants/mixins.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/apps/tenants/mixins.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.core.exceptions import PermissionDenied
+
+
+class TenantAccessRequiredMixin(AccessMixin):
+    """CBV mixin which verifies that the current user is authenticated."""
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if request.user.is_anonymous():
+            return self.handle_no_permission()
+
+        # TODO: Only out so that Lorenz can work on the Comments API
+
+        if request.tenant is None or not request.user.tenants.filter(
+                pk=request.tenant.id):
+            return self.handle_no_permission()
+
+        print("SiteAccessRequiredMixin: User {} on tenant {} okay!".format(
+            request.user, request.tenant))
+
+        return super(TenantAccessRequiredMixin, self).dispatch(
+            request, *args, **kwargs)
+
+    def handle_no_permission(self):
+        raise PermissionDenied(self.get_permission_denied_message())
