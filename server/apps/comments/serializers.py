42f56547f45e6ec95cc5aceb0062ee9ad143150d
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:23 +0100

    commit changes to server/apps/comments/serializers.py
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ server/apps/comments/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Comment
+
+
+class CommentSerializer(ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('content_type', 'object_id',
+                  'author', 'content', 'time_created')
+        read_only_fields = ('time_created',)
