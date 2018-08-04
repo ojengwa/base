0012875159e16141c1f9b83d5101a33f7c5a4673
Bernard Ojengwa <bernardojengwa@gmail.com>
2018-08-04 22:26:15 +0100

    commit changes to client/src/frontend/actions/auth.js
    Signed-off-by: Bernard Ojengwa <bernardojengwa@gmail.com>

---
+++ client/src/frontend/actions/auth.js
@@ -0,0 +1,93 @@
+import fetch from 'isomorphic-fetch';
+import { push } from 'react-router-redux';
+import { SERVER_URL } from '../utils/config';
+import { checkHttpStatus, parseJSON } from '../utils';
+import {
+    AUTH_LOGIN_USER_REQUEST,
+    AUTH_LOGIN_USER_FAILURE,
+    AUTH_LOGIN_USER_SUCCESS,
+    AUTH_LOGOUT_USER
+} from '../constants';
+
+
+export function authLoginUserSuccess(token, user) {
+    sessionStorage.setItem('token', token);
+    sessionStorage.setItem('user', JSON.stringify(user));
+    return {
+        type: AUTH_LOGIN_USER_SUCCESS,
+        payload: {
+            token,
+            user
+        }
+    };
+}
+
+export function authLoginUserFailure(error, message) {
+    sessionStorage.removeItem('token');
+    return {
+        type: AUTH_LOGIN_USER_FAILURE,
+        payload: {
+            status: error,
+            statusText: message
+        }
+    };
+}
+
+export function authLoginUserRequest() {
+    return {
+        type: AUTH_LOGIN_USER_REQUEST
+    };
+}
+
+export function authLogout() {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('user');
+    return {
+        type: AUTH_LOGOUT_USER
+    };
+}
+
+export function authLogoutAndRedirect() {
+    return (dispatch, state) => {
+        dispatch(authLogout());
+        dispatch(push('/login'));
+        return Promise.resolve(); // TODO: we need a promise here because of the tests, find a better way
+    };
+}
+
+export function authLoginUser(email, password, redirect = '/') {
+    return (dispatch) => {
+        dispatch(authLoginUserRequest());
+        const auth = btoa(`${email}:${password}`);
+        return fetch(`${SERVER_URL}/api/v1/accounts/login/`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${auth}`
+            }
+        })
+            .then(checkHttpStatus)
+            .then(parseJSON)
+            .then((response) => {
+                dispatch(authLoginUserSuccess(response.token, response.user));
+                dispatch(push(redirect));
+            })
+            .catch((error) => {
+                if (error && typeof error.response !== 'undefined' && error.response.status === 401) {
+                    // Invalid authentication credentials
+                    return error.response.json().then((data) => {
+                        dispatch(authLoginUserFailure(401, data.non_field_errors[0]));
+                    });
+                } else if (error && typeof error.response !== 'undefined' && error.response.status >= 500) {
+                    // Server side error
+                    dispatch(authLoginUserFailure(500, 'A server error occurred while sending your data!'));
+                } else {
+                    // Most likely connection issues
+                    dispatch(authLoginUserFailure('Connection Error', 'An error occurred while sending your data!'));
+                }
+
+                return Promise.resolve(); // TODO: we need a promise here because of the tests, find a better way
+            });
+    };
+}
